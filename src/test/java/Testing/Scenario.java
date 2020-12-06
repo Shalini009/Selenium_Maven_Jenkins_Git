2b2d048c8d9af297031f3cd1c93c3148b2657176
Shalini009 <65846189+Shalini009@users.noreply.github.com>
2020-12-05 21:25:05 -0500

    Update Scenario.java

--- src/test/java/Testing/Scenario.java
+++ src/test/java/Testing/Scenario.java
@@ -1,10 +1,18 @@
 package Testing;
 
-
-
 public class Scenario {
 
 
+	public static void main(String[] args) {
+		System.out.println(new Application().getMessage());
+	}
+
+	private final String message = "Welcome to Simplilearn !!! Jenkins WebHooks";
+
+	private String getMessage() {
+		
+		return message ;
+	}
 	}
 
 
